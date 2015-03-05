c0d6fc3ad4f4d7beeb5694e62b222bb2aa99fe06
Kjell H Andersen <kjell@lentic.net>
2015-03-05 16:07:32 +0100

    Starting to add some SD card code

---
+++ src/vhdl/sd_spi/sd_spi_pkg.vhd
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/05/2015 03:03:28 PM
+-- Design Name: 
+-- Module Name: sd_spi_pkg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package SdSpi_pkg is
+
+  type CardType_t is (SD_CARD_E, SDHC_CARD_E);  -- Define the different types of SD cards.
+
+end package;
